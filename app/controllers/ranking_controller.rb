3f733d6de79a26d5266a2c9249f677b4899931af
Neku <shinya.komatsu@kingdevice.com>
2017-01-08 13:06:38 +0000

    add ranking list

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,38 @@
 class RankingController < ApplicationController
   def have
+    array = Array.new
+    Item.find_each(batch_size: 100) do |item|
+      userCount = item.have_user_count
+      if userCount > 0
+        rankItem = {:count => userCount, :item => item}
+        array.push(rankItem)
+      end
+    end
+    sliceCount = array.count > 10? 10 : array.count
+    rankItems = array.sort_by{|val| [-val[:count], -val[:count]]}.slice!(0, sliceCount)
+    topRanks = Array.new
+    rankItems.each do |item|
+      topRanks.push(item[:item])
+    end
+    @items = topRanks
   end
 
   def want
+    array = Array.new
+    Item.find_each(batch_size: 100) do |item|
+      userCount = item.want_user_count
+      if userCount > 0
+        rankItem = {:count => userCount, :item => item}
+        array.push(rankItem)
+      end
+    end
+    sliceCount = array.count > 10? 10 : array.count
+    rankItems = array.sort_by{|val| [-val[:count], -val[:count]]}.slice!(0, sliceCount)
+    topRanks = Array.new
+    rankItems.each do |item|
+      topRanks.push(item[:item])
+    end
+    @items = topRanks
   end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,12 @@ class Item < ActiveRecord::Base
   has_many :want_users , through: :wants, source: :user
   has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
   has_many :have_users , through: :haves, source: :user
+  
+  def have_user_count
+    have_users.count
+  end
+  
+  def want_user_count
+    want_users.count
+  end
 end
